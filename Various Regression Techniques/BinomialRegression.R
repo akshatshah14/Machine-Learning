5a6c8b23190d5e9dfc4a8f0931e8e4fa201727c7
akshatshah14 <akshat_shah14@yahoo.com>
2017-10-09 01:28:48 -0400

    Update BinomialRegression.R

--- Various Regression Techniques/BinomialRegression.R
+++ Various Regression Techniques/BinomialRegression.R
@@ -1,14 +1,4 @@
 
-#===============================================================================================================================
-#Binomial Regression
-#Machine Learning for Data Science--spring 2017
-#Instructor: Nabil R. Adam (adam@adam.rutgers.edu)
-#Author: debopriya.ghosh@rutgers.edu
-#Last Modified On:
-#===============================================================================================================================
-
-
-
 install.packages("ISLR");
 library(ISLR)
 
@@ -36,7 +26,7 @@ summary(glm.fit)
 #(Intercept) -3.094e+00  1.463e-01 -21.156   <2e-16 ***
 #income      -8.353e-06  4.207e-06  -1.985   0.0471 *  
 #---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#Signif. codes:  0 â€˜***â€™ 0.001 â€˜**â€™ 0.01 â€˜*â€™ 0.05 â€˜.â€™ 0.1 â€˜ â€™ 1
 
 #(Dispersion parameter for binomial family taken to be 1)
 
